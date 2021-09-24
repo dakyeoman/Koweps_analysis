7832d265771b02878fee3478913c230419d04c75
Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-24 23:38:51 +0900

    Add files via upload

---
+++ 1stProject_Korean_welfare.R
@@ -0,0 +1,41 @@
+
+install.packages("foreign")
+library(foreign)
+library(dplyr)
+library(ggplot2)
+library(readxl)
+
+install.packages("haven")
+library(haven)
+
+raw_welfare <- read.spss("desktop/Koweps_Analysis/Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
+welfare <- raw_welfare
+
+head(welfare)
+tail(welfare)
+View(welfare)
+dim(welfare)
+str(welfare)
+summary(welfare)
+
+welfare <- rename(welfare, 
+                  sex = h10_g3, 
+                  birth = h10_g4, 
+                  marriage = h10_g10, 
+                  religion = h10_g11, 
+                  income = p1002_8aq1, 
+                  code_job = h10_eco9, 
+                  code_region = h10_reg7)
+
+#성별 변수 검토, 전처리
+
+class(welfare$sex)
+table(welfare$sex)
+#이상치 결측 처리: 
+welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
+
+#성별 항목 이름 부여
+welfare <- ifelse(welfare$sex == 1, "male", "female")
+table(welfare)
+qplot(welfare$sex)
+
