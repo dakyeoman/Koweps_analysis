7730bc21e10e11ddb5fc4323ceafef4f1c60c44a
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-25 18:43:52 +0900

    Update 1stProject_Korean_welfare.R
    I'm trying to find why I can't use $ operater.

--- 1stProject_Korean_welfare.R
+++ 1stProject_Korean_welfare.R
@@ -1,16 +1,19 @@
 
 install.packages("foreign")
 install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("readxl")
+install.packages("haven") 
+
 library(foreign)
 library(dplyr)
 library(ggplot2)
 library(readxl)
-
-install.packages("haven")
 library(haven)
 
 raw_welfare <- read.spss("desktop/Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
 welfare <- raw_welfare
+
 
 head(welfare)
 tail(welfare)
@@ -19,19 +22,23 @@ dim(welfare)
 str(welfare)
 summary(welfare)
 
-welfare <- rename(welfare, 
-                  sex = h10_g3, 
-                  birth = h10_g4, 
-                  marriage = h10_g10, 
-                  religion = h10_g11, 
-                  income = p1002_8aq1, 
-                  code_job = h10_eco9, 
-                  code_region = h10_reg7)
+welfare <-rename(welfare, 
+       sex = h10_g3,
+       birth = h10_g4, 
+       marriage = h10_g10, 
+       religion = h10_g11, 
+       income = p1002_8aq1, 
+       code_job = h10_eco9, 
+       code_region = h10_reg7)
 
+#welfare.data <- as.data.frame(welfare)
+#x <-data.frame(welfare)
+#class(x)
 #성별 변수 검토, 전처리
-data %>% dplyr::summarise()#모르겠다!
+#data %>% dplyr::summarise()#모르겠다!
 
 class(welfare$sex)
+
 
 table(welfare$sex)
 #이상치 결측 처리: 
@@ -42,9 +49,19 @@ welfare <- ifelse(welfare$sex == 1, "male", "female")
 table(welfare)
 qplot(welfare)
 
+
+#자 난 이걸 dataframe으로 바꿀거야. 
+class(welfare) #문자열
+
+welfare <-as.data.frame(welfare)
+class(welfare)
+
+welfare$income
 is.recursive(welfare)
 class(welfare$income)
+#왜 $로 찾을 수가 없지?
 
+#$ operater invalid오류를 해결하기 위한 몸부림(아래) 
 x <- c(1, 2)
 x
 names(x) <- c("bob", "ed")
@@ -53,4 +70,13 @@ is.atomic(x) #T
 is.recursive(x) #F
 
 x["ed"] #2
+getElement(x, "ed")
+
+
+x <- c(1, 2)
+x
+names(x) <- c("bob", "ed")
+x <- as.data.frame(t(x))
+x$ed
+class(x)
 
