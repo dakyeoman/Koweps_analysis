1cc0f5ad25b70021fe6a8cabfef99eac01ec892d
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-25 18:06:28 +0900

    Trying to preprocess data

--- 1stProject_Korean_welfare.R
+++ 1stProject_Korean_welfare.R
@@ -1,5 +1,6 @@
 
 install.packages("foreign")
+install.packages("ggplot2")
 library(foreign)
 library(dplyr)
 library(ggplot2)
@@ -8,12 +9,12 @@ library(readxl)
 install.packages("haven")
 library(haven)
 
-raw_welfare <- read.spss("desktop/Koweps_Analysis/Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
+raw_welfare <- read.spss("desktop/Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
 welfare <- raw_welfare
 
 head(welfare)
 tail(welfare)
-View(welfare)
+#View(welfare)
 dim(welfare)
 str(welfare)
 summary(welfare)
@@ -28,8 +29,10 @@ welfare <- rename(welfare,
                   code_region = h10_reg7)
 
 #성별 변수 검토, 전처리
+data %>% dplyr::summarise()#모르겠다!
 
 class(welfare$sex)
+
 table(welfare$sex)
 #이상치 결측 처리: 
 welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
@@ -37,5 +40,17 @@ welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
 #성별 항목 이름 부여
 welfare <- ifelse(welfare$sex == 1, "male", "female")
 table(welfare)
-qplot(welfare$sex)
+qplot(welfare)
+
+is.recursive(welfare)
+class(welfare$income)
+
+x <- c(1, 2)
+x
+names(x) <- c("bob", "ed")
+x$ed
+is.atomic(x) #T
+is.recursive(x) #F
+
+x["ed"] #2
 
