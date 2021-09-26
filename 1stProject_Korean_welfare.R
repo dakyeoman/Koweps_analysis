c305520ec0f4bdeee1457eaf53ecc83ea112293c
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-09-26 22:58:48 +0900

    Update 1stProject_Korean_welfare.R

--- 1stProject_Korean_welfare.R
+++ 1stProject_Korean_welfare.R
@@ -13,7 +13,8 @@ library(haven)
 
 raw_welfare <- read.spss("desktop/Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
 welfare <- raw_welfare
-
+welfare2 <- as.data.frame(welfare)
+class(raw_welfare)
 
 head(welfare)
 tail(welfare)
@@ -39,10 +40,11 @@ welfare <-rename(welfare,
 
 class(welfare$sex)
 
-
-table(welfare$sex)
 #이상치 결측 처리: 
-welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
+table(welfare$sex)
+#welfare$sex <- ifelse(welfare$sex == 9, NA, welfare$sex)
+#table(is.na(welfare$sex))
+
 
 #성별 항목 이름 부여
 welfare <- ifelse(welfare$sex == 1, "male", "female")
@@ -51,17 +53,19 @@ qplot(welfare)
 
 
 #자 난 이걸 dataframe으로 바꿀거야. 
-class(welfare) #문자열
-
-welfare <-as.data.frame(welfare)
-class(welfare)
+#class(welfare) #문자열
+#welfare <-as.data.frame(welfare)
+#class(welfare)
+#안 됨.
 
 welfare$income
 is.recursive(welfare)
 class(welfare$income)
-#왜 $로 찾을 수가 없지?
+welfare["income"]
 
+#(위) 왜 $로 찾을 수가 없지? 월급변수 검토 과정 일단 생략해보자. 
 #$ operater invalid오류를 해결하기 위한 몸부림(아래) 
+
 x <- c(1, 2)
 x
 names(x) <- c("bob", "ed")
@@ -71,12 +75,15 @@ is.recursive(x) #F
 
 x["ed"] #2
 getElement(x, "ed")
-
-
+x <- as.data.frame(x)
+x$ed 
+#어???
 x <- c(1, 2)
 x
 names(x) <- c("bob", "ed")
 x <- as.data.frame(t(x))
 x$ed
 class(x)
+
+#안 되겠다 기초부터 해야 쓰겠다.
 
