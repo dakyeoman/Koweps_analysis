b1363ad31e49244f11051da4f88c7b15cf838b4e
An Dakyeom <63940054+dakyeoman@users.noreply.github.com>
2021-10-23 23:33:46 +0900

    Update Part2_EasyRDataAnalysis.R

--- Part2_EasyRDataAnalysis.R
+++ Part2_EasyRDataAnalysis.R
@@ -269,3 +269,21 @@ mpg %>%
   filter(!is.na(hwy)) %>%
   group_by(drv) %>%
   summarise(mean_hwy = mean(hwy))
+
+
+#7-2 이상치 정제하기
+outlier <- data.frame(sex = c(1, 2, 1, 3, 2, 1), 
+                      score = c(5, 4, 3, 4, 2, 6)) ;outlier
+#이상치 확인
+table(outlier$sex)
+table(outlier$score)
+
+#결측처리
+outlier$sex <- ifelse(outlier$sex == 3, NA, outlier$sex);outlier
+outlier$score <- ifelse(outlier$score == 6, NA, outlier$score);outlier
+
+#분석 시 결측치 제외
+outlier %>%
+  filter(!is.na(sex) & !is.na(score)) %>%
+  group_by(sex) %>%
+  summarise(mean_score = mean(score))
